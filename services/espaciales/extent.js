71cae9235abd1d94a5f1b3e43b476edb6c55eee6
Bryan <bcarbajal@ideasg.org>
2024-01-11 08:59:06 -0500

    error de extent

--- services/espaciales/extent.js
+++ services/espaciales/extent.js
@@ -16,8 +16,8 @@ export class ExtentService {
     async getExtentTable(tabla) {
         try {
         // const distrito = await SpDistrito.findAll();
-        const dbResponse = await sequelize.query(`SELECT CAST(ST_Extent(ST_Transform(glgis, 3857)) AS VARCHAR) as bextent FROM 
-        (SELECT * FROM espaciales.${tabla}) as t`);
+        console.log(`SELECT CAST(ST_Extent(ST_Transform("IDEASG" , 3857)) AS VARCHAR) as bextent from espaciales.${tabla})`);
+        const dbResponse = await sequelize.query(`SELECT CAST(ST_Extent(ST_Transform("IDEASG" , 3857)) AS VARCHAR) as bextent from espaciales.${tabla}`);
         return dbResponse;
         } catch (error) {
         throw new Error("Error al obtener la extensión.");
