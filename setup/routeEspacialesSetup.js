367dcae2e55a37172ecb9bb560b8b8fa335ca8e1
RENZO GARCIA AUQUI <rgarcia@ideasg.org>
2024-01-31 10:31:29 -0500

    Update routeEspacialesSetup.js

--- setup/routeEspacialesSetup.js
+++ setup/routeEspacialesSetup.js
@@ -5,15 +5,15 @@ import { createRouteViasCuadras } from "../routes/espaciales/vias_cuadras.js";
 import { createRouteMapaBase } from "../routes/espaciales/mapa_base.js";
 
 export function setupEspacialesRoutes(app) {
-    const maestrosRutas = {
-        capas: createRouteCapas(),
-        sml: createRouteSML(),
-        extent: createRouteExtent(),
-        vias_cuadras: createRouteViasCuadras(),
-        mapa_base: createRouteMapaBase()
-    };
+  const maestrosRutas = {
+    capas: createRouteCapas(),
+    sml: createRouteSML(),
+    extent: createRouteExtent(),
+    vias_cuadras: createRouteViasCuadras(),
+    mapa_base: createRouteMapaBase(),
+  };
 
-    for (const [ruta, router] of Object.entries(maestrosRutas)) {
-        app.use(`/espaciales/${ruta}`, router);
-    }
+  for (const [ruta, router] of Object.entries(maestrosRutas)) {
+    app.use(`/espaciales/${ruta}`, router);
+  }
 }
