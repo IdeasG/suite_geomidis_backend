993ec2fca7212db64c583bb527cce5df250830a9
IdeasG Developer Agent <devagent@ideasg.org>
2026-06-15 13:24:16 -0500

    config: update CORS to allow geovisormidis.ideasg.org

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -3,7 +3,7 @@ import cors from "cors";
 const ACCEPTED_ORIGINS = [
   "http://localhost:3000",
   "https://geoportal.midis.gob.pe",
-  "https://geomidis23.ideasg.org"
+  "https://geovisormidis.ideasg.org"
 ];
 
 export const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) => (req, res, next) => {
